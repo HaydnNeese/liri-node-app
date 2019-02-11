4f174cc8113f092b7205a9b17e2a6a9d3f156c5c
HaydnNeese <haydnneese@yahoo.com>
2019-02-10 18:18:49 -0600

    finished project

--- README.md
+++ README.md
@@ -1,3 +1,86 @@
-# liri-node-app
+# **LIRI-NODE-APP**
 
-In order to run app the user must create their very own .env file containing the keys: ID and Secret
+**-----IMPORTANT-----**
+
+*In order to use this app you must you must first make your own .env file* 
+
+---
+## **HOW IT WORKS**
+---
+The Liri-Node-App is a CLI app that receives user input and provides it to three separate API's to retrieve information about: 
+
+
+*       Song information through the use of the Spotify API
+    * Required syntax:
+        * **spotify-this-song** < *song name* >
+    * If the user fails to type **spotify-this-song** correctly or at all the program will remind the user of how to use the correct syntax
+    * If the user types **spotify-this-song** and does not put a song name the program will automatically enter a song name for them
+    *   The program will write the API information to a local file called log.txt
+
+*       Concert information through the use of the Bands In Town API
+    * required syntax:
+        *  **concert-this** < *band name* >
+    * If the user fails to type **concert-this** correctly or at all the program will remind the user of how to use the correct syntax
+    * If the user types **concert-this** and does not put a song name then the program will automatically enter a band name for them
+    *   The program will write the API information to a local file called log.txt
+
+*        Movie information through the use of the OMDB API
+    * required syntax:
+        * **movie-this** < *movie name* >
+    * If the user fails to type **movie-this** correctly or at all the program will remind the user of how to use the correct syntax 
+    * If the user types **movie-this** and does not put a song name then the program will automatically enter a movie name for them
+    *   The program will write the API information to a local file called log.txt
+
+*       If the user inputs **do-what-it-says** it will read a local .txt file to run the spotify-this-song command for "I Want It That Way"
+    *   The program will write the API information to a local file called log.txt
+
+---
+## **HOW IT IS USEFUL**
+---
+
+        The CLI application allows the user to quickly find information on songs, bands, and movies by quickly typing into the terminal and receiving neatly organized search results.
+---
+## **VIDEO EXAMPLE**
+
+[![Watch the video](video_img.png)](https://drive.google.com/file/d/1GuS4iNMMljZuZSrzDzsfg8OXWWchHh3H/view)
+
+---
+## **PICTURE EXAMPLES**
+---
+### ERROR MESSAGE EXAMPLE
+![error message](images/error.png)
+
+---
+### SPOTIFY NO SONG
+![spotify](images/spot1.png)
+
+---
+### SPOTIFY W/ SONG
+![spotify](images/spot2.png)
+
+---
+### CONCERT NO BAND
+![concert](images/concert1.png)
+
+---
+### CONCERT W/ BAND
+![concert](images/concert2.png)
+
+---
+### MOVIE NO TITLE
+![movie](images/movie1.png)
+
+---
+### MOVIE W/ TITLE
+![movie](images/movie2.png)
+
+---
+### DO WHAT IT SAYS
+![dowhatitsays](images/do_what_it_says.png)
+
+
+
+
+
+
+
--- liri.js
+++ liri.js
@@ -1,86 +1,262 @@
+//set up programs with the require methods
 require("dotenv").config();
 var Spotify = require('node-spotify-api');
 var keys = require("./keys.js");
 var axios = require("axios");
 var moment = require("moment");
-// var terminalLink = require('terminal-link');
-
+var fs = require("fs");
 var spotify = new Spotify(keys.spotify);
+
+
 //create a variable for the request text
 var request = process.argv[2];
 // input is the specifics that follows the request... song name, band name, movie name, etc.
 var input = process.argv;
 // create an empty array to be pushed into
 var inputArray = [];
+
+
 //all items after index 2 in the process.argv array need pushed into the inputArray
-for(var i = 3; i < input.length; i++) {
+for (var i = 3; i < input.length; i++) {
     inputArray.push(input[i]);
 };
 
-var queryURL = "https://rest.bandsintown.com/artists/" + inputArray.join('') + "/events?app_id=codingbootcamp&date=upcoming"
+//make a function to make a random command to the terminal
+function randomCommand() {
+    var commandArray = ["spotify-this-song", "concert-this", "movie-this"];
+    var x = Math.floor(Math.random() * Math.floor(3));
+    fs.readFile("random.txt", "utf8", function (error, data) {
+        if (error) {
+            console.log(error);
+        }
+        var dataArr = data.split(",");
+        request = dataArr[0];
+        input = dataArr[1];
+        inputArray = input.split(" ");
+        console.log(inputArray);
+        if (request === commandArray[0]) {
+            spotifyThis();
+        } else if (request === commandArray[1]) {
+            concertThis();
+        } else if (request === commandArray[2]) {
+            movieThis();
+        }
+    });
+};
 
-//store the search requirements into one variable
-var spotifyOptions = {
-    type: 'track',
-    query: inputArray,
-    limit: 1
-}
-//if the request from the user starts with this string run the spotify API to retrieve info about that song
-if (request === "spotify-this-song") {
-    console.log("Searching for information on your song \n")
-
+//store SPOTIFY API into a function
+function spotifyThis() {
+    var spotifyOptions = {
+        type: 'track',
+        query: inputArray,
+        limit: 1
+    };
+    console.log("\n-------------------------------------");
+    console.log("Searching for information on your song.");
+    console.log("-------------------------------------\n");
     spotify
         .search(spotifyOptions)
         .then(function (response) {
+            //assign a base variable to simplify the response
+            var song = response.tracks.items[0];
             //assign variable to different parts of the response
-            var artistName = response.tracks.items[0].artists[0].name;
-            var songTitle = response.tracks.items[0].name;
-            var link = response.tracks.items[0].preview_url;
-            var album = response.tracks.items[0].album.name;
-            //when a song doesn't have a preview make that clear to the user
-            if(link === null) {
+            var artistName = song.artists[0].name;
+            var songTitle = song.name;
+            var link = song.preview_url;
+            var album = song.album.name;
+            //when a song doesn't have a preview make that clear to the user with a console log No Preview Available
+            if (link === null) {
                 link = "No Preview Available";
             }
-            // console.log(JSON.stringify(response.tracks.items[0].album.name, null, 2));
+            var songData = [
+                "\n\nArtist Name: " + artistName,
+                "Song Title: " + songTitle,
+                "Album: " + album,
+                "Preview: " + link
+            ].join("\n\n");
             //cleanly layout the console.log for the user
-            console.log("Artist Name: " + artistName + "\n" +
-                "\nSong Title: " + songTitle + "\n" +
-                "\nAlbum: " + album + "\n" +
-                "\nPreview: " + link);
-
+            console.log(songData);
+            fs.appendFile("log.txt", songData, function (err) {
+                // If an error was experienced we will log it.
+                if (err) {
+                    console.log(err);
+                }
+                // If no error is experienced, we'll log the phrase "Content Added" to our node console.
+                else {
+                    console.log("Content Added!");
+                }
+            });
         })
+        //watch for error and if error throw an error message
         .catch(function (err) {
             console.log(err);
         });
-}else if (request === "concert-this") {
+};
+
+
+
+//store Bands-in-Town API in a function 
+function concertThis() {
     //notify the program is searching for info
-    console.log("Looking for concerts performed by this band..." + "\n")
-    // console.log(queryURL);
-    axios.get(queryURL).then(
+    console.log("\n------------------------------------------------");
+    console.log("Looking for concerts performed by this band.");
+    console.log("------------------------------------------------\n");
+    //make sure to join the array so the URL can be read
+    axios.get("https://rest.bandsintown.com/artists/" + inputArray.join('') + "/events?app_id=codingbootcamp&date=upcoming").then(
         function (response) {
+            //assign data location to a variable
+            var eventData = response.data;
             //organize response data into variables
-            var venueCity = response.data[0].venue.city;
-            var venueRegion = response.data[0].venue.region;
-            var venueName = response.data[0].venue.name;
-            //format the time to MM/DD/YYYY
-            var eventDate = moment(response.data[0].datetime).format("MM/DD/YYYY");
-            // console.log(response);
-            //write to the terminal an organzied list of concert information
-            console.log("Venue: " + venueName + "\n" + 
-            "\nLocation: " + venueCity + ", " + venueRegion + "\n" + 
-            "\nDate of Event: " + eventDate);
+            for (i = 0; i < 3; i++) {
+                var venueCity = eventData[i].venue.city;
+                if (eventData[i].venue.region === !null) {
+                    var venueRegion = eventData[i].venue.region;
+                } else {
+                    var venueRegion = eventData[i].venue.country;
+                }
+                var venueName = eventData[i].venue.name;
+                var space = "\n\n\n";
+                //format the time to MM/DD/YYYY
+                var eventDate = moment(response.data[i].datetime).format("MM/DD/YYYY");
+                var concertData = [
+                    "\n\nBand Name: " + inputArray.join(" "),
+                    "Venue: " + venueName,
+                    "Location: " + venueCity + ", " + venueRegion,
+                    "Date of Event: " + eventDate + space + "\n"
+                ].join("\n\n");
+                // write to the terminal a"n organzied list of concert information
+                console.log(concertData);
+                fs.appendFile("log.txt", concertData, function (err) {
+                    // If an error was experienced we will log it.
+                    if (err) {
+                        console.log(err);
+                    }
+                    // If no error is experienced, we'll log the phrase "Content Added" to our node console.
+                    else {
+                        console.log("Content Added!");
+                    }
+                });
+            };
         }
-    )
-}else if(request === "movie-this") {
-    //write OMDB code
+        //watch for and throw error if it happens
+    ).catch(function (err) {
+        console.log(err);
+    });
+};
 
 
-}else {
-    //if the user types the command wrong remind them about this text to initialize a spotify search
-    console.log("If looking for a song, enter 'spotify-this-song' before the song title." + 
-                "\nIf looking for concert locations for your favorite band then enter 'concert-this' before your band name." + 
-                "\nIf looking for info on your favorite movie enter 'movie-this' before the name of the movie." + 
-                "\nIf you want it that way enter 'do-what-it-says' and press enter.");
+//store OMDB API into 
+function movieThis() {
+    var url = "http://www.omdbapi.com/?t=" + inputArray.join('&') + "&y=&plot=short&apikey=trilogy";
+    //if the user did provide a movie name
+    console.log("\n------------------------------------");
+    console.log("Looking for info on that movie..." +
+        "\n-------------------------\n");
+    axios.get(url).then(
+        function (response) {
+            //store response data into var movie
+            var movie = response.data;
+            //write reponse data into a var moviedata neatly organized to be displayed in the terminal
+            var movieData = [
+                "\n\nTitle: " + movie.Title,
+                "Release Year: " + movie.Year,
+                "IMDB Rating: " + movie.imdbRating,
+                "Rotten Tomatoes Rating: " + movie.Ratings[1].Value,
+                "Country: " + movie.Country,
+                "Language: " + movie.Language,
+                "Plot: " + movie.Plot,
+                "Actors: " + movie.Actors + "\n"
+            ].join("\n\n");
+            console.log(movieData);
+            fs.appendFile("log.txt", movieData, function (err) {
+                // If an error was experienced we will log it.
+                if (err) {
+                    console.log(err);
+                }
+                // If no error is experienced, we'll log the phrase "Content Added" to our node console.
+                else {
+                    console.log("Content Added!");
+                }
+            });
+        }
+    ).catch(function (err) {
+        console.log(err);
+    });
+}
+
+
+/**Spotify API */
+//if the request from the user starts with this string run the spotify API to retrieve info about that song
+if (request === "spotify-this-song") {
+    //check to make sure that inputArray is not empty
+    if (inputArray.length < 1) {
+        //if it is empty insert The Sign into the array
+        //console.log to the user that they did not choose a song and you will choose one for them
+        console.log("\n-------------------------------------");
+        console.log("Looks like you did not choose a song\n" +
+            "\nI'll choose one for you\n" +
+            "\nHow about The Sign?\n" +
+            "\nEver heard that one?.... Me either.");
+        inputArray = ["The", "Sign"];
+        //run the spotify api
+        spotifyThis();
+    } else {
+        //if the inputarray is not empty then use its contents to run the spotify api
+        spotifyThis();
+    };
+
+
+    /**Bands In Town API */
+    //watch for concert-this command in the processargv[2] location of the command line
+} else if (request === "concert-this") {
+    //make sure the input array isn't empty
+    if (inputArray.length < 1) {
+        //talk to the user letting them know you are choosing a band for them
+        console.log("\n-------------------------");
+        console.log("You didn't choose a band!\n" +
+            "\nI'll Choose a Band For You!\n" +
+            "How about.... John Mayer" +
+            "\n-------------------------\n");
+        //assign John Mayer to the band name
+        inputArray = ["John", "Mayer"];
+        concertThis();
+    } else {
+        concertThis();
+    };
+
+
+    /**OMDB API */
+} else if (request === "movie-this") {
+    //check the array is not empty
+    if (inputArray.length < 1) {
+        //if it is fill it with Mr. Nobody
+        inputArray = ["Mr.", "Nobody"];
+        //talk to user to explain they did not pick a movie and tell them the movie you're choosing for them
+        console.log("\n------------------------------");
+        console.log("You forgot to choose a movie\n" +
+            "\nWe will pick for you....\n" +
+            "\nHow about... Mr. Nobody" +
+            "\n-------------------------\n");
+        movieThis();
+    } else {
+        movieThis();
+    }
+
+
+} else if (request === "do-what-it-says") {
+    randomCommand();
+}
+else {
+    //if the user types their search incorrectly remind them of the syntax to ask the program the specific question
+    console.log("\nI'm sorry, you entered the command incorrectly please remember: \n" +
+        "\n---------------------------------------------------------------------------" +
+        "\nIf looking for a song, enter 'spotify-this-song' before the song title." +
+        "\n-----------------------------------------------------------------------------------" +
+        "\nIf looking for concert locations for your favorite band then enter 'concert-this' before your band name." +
+        "\n-----------------------------------------------------------------------------------" +
+        "\nIf looking for info on your favorite movie enter 'movie-this' before the name of the movie." +
+        "\n-----------------------------------------------------------------------------------" +
+        "\nIf you want it that way..... enter 'do-what-it-says' and press enter.");
 };
 
 
